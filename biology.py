9e0cf61903256f7bf295a8719e24cfbf41e395b3
francisco laptop <serrfrancisco@yahoo.com>
2023-12-01 17:07:44 -0800

    finished target page function

--- biology.py
+++ biology.py
@@ -6,22 +6,38 @@ from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 from sklearn.feature_extraction.text import TfidfVectorizer
+from typing import List
 
 
-#Connect to MongoDB
-client = MongoClient('localhost', 27017)
-db = client['biology']
-pages_collection = db.pages
+def main():
+    # Connect to MongoDB
+    seed_urls = ['https://www.cpp.edu/sci/biological-sciences/index.shtml']
+    frontier = CrawlerFrontier(seed_urls)
+    num_targets = 10
+    crawlerThread(frontier, num_targets)
 
-frontier = ['https://www.cpp.edu/sci/biological-sciences/index.shtml']
-visited_links = set()
-num_targets = 10
 
-#stores pages
+class CrawlerFrontier:
+    def __init__(self, seed_frontier: List[str]):
+        self.frontier = seed_frontier
+        self.visited_links = set()
+# stores pages
+
+
 def storePages(url, html):
-    pages_collection.insert_one({"url" : url, 'html' : html})
+    try:
+        client = MongoClient('localhost', 27017)
+        db = client['biology']
+        pages_collection = db.pages
+    except Exception:
+        print("unable to connect to db")
+    try:
+        pages_collection.insert_one({"url": url, 'html': html})
+    except Exception:
+        print("error adding the page to db")
 
-#retrieves URL
+
+# retrieves URL
 def retrieveUrl(url):
     try:
         html = urlopen(url).read()
@@ -31,12 +47,20 @@ def retrieveUrl(url):
     except URLError as e:
         print('The server could not be found!')
 
-#target page
+
+# target page
 def targetPage(html):
-    #Check if it is target page
+    # Check if it is target page
+    bs = BeautifulSoup(html, 'html.parser')
+    if (bs.find('ul', {'class': 'fac-nav'})
+            and bs.find('div', {'class': 'fac-staff'})):
+        return True
+    return False
+
     pass
 
-#parsing
+
+# parsing
 def parse(html, baseUrl):
     bs = BeautifulSoup(html, 'html.parser')
     links = []
@@ -48,19 +72,29 @@ def parse(html, baseUrl):
 
     return links
 
-#crawling
-def crawlerThread(frontier, num_targets):
+
+# crawling
+def crawlerThread(frontier: CrawlerFrontier, num_targets: int):
     targets_found = 0
-    while frontier != 0:
+    visited_links = frontier.visited_links
+    frontier = frontier.frontier
+    while len(frontier) != 0:
         url = frontier.pop(0)
         visited_links.add(url)
         html = retrieveUrl(url)
-        storePages(url, html)
-        if targetPage(html):
-            targets_found += 1
-            if targets_found == num_targets:
-                frontier.clear()
-        else:
-            for newUrl in parse(html, url):
-                if newUrl not in visited_links and newUrl not in frontier:
-                    frontier.append(newUrl)
+        # returns none if bad html
+        if (html):
+            storePages(url, html)
+            if targetPage(html):
+                print("found a target page: ", url)
+                targets_found += 1
+                if targets_found == num_targets:
+                    frontier.clear()
+            else:
+                for newUrl in parse(html, url):
+                    if newUrl not in visited_links and newUrl not in frontier:
+                        frontier.append(newUrl)
+
+
+if __name__ == "__main__":
+    main()
